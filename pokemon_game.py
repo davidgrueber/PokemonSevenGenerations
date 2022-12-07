1d8f2d9882650d041d3940241864115fd218dc95
sjgandhi16 <114253242+sjgandhi16@users.noreply.github.com>
2022-12-07 01:37:50 -0800

    Update pokemon_game.py

--- pokemon_game.py
+++ pokemon_game.py
@@ -1,77 +1,192 @@
 import numpy as np
 import pandas as pd
-import ipywidgets as widgets
-import ipywidgets as widgets
+import matplotlib.image as mpimg
+import random
 
-import pandas as pd
-
-pokemonData = pd.read_csv("pokemon.csv")
-pokemonData = pokemonData[:10]
-
-def on_button_clicked(Button, playerNum):
-    with outputs[playerNum]:
-        print(f"Player {playerNum+1} chose {Button.description}!")
-        print(pokemonData.loc[pokemonData['name'] == Button.description])#['hp'].values[0])
-        desiredPokemonNames.append(Button.description)
-        readPokemonStats(player1pokemon, Button.description)
+def display_pokemon(my_poke1, current_hp1, my_poke2, current_hp2):
+    '''
+    Displays the competing Pokemon and their HP bars.
+    Args:
+        my_poke1: string with the name of Player 1's Pokemon
+        current_hp1: Player 1's Pokemon's current health (string or int)
+        my_poke2: string with Player 2's Pokemon
+        current_hp2: Player 2's Pokemon's current health (string or int)
+    Returns:
+        None
+    Example:
+        my_poke1: "pikachu"
+        my_poke2: "charizard"
+        Function will display pixelart sprite of Pikachu and Charizard, with an HP bar of their current health
+        Health cannot exceed 255 or fall below 0.
+    '''
+    try:
+        # set pokemon names to lowercase
+        my_poke1 = my_poke1.lower()
+        my_poke2 = my_poke2.lower()
+        # set maximum health to integer value of each pokemon's health from data
+        max_hp1 = int(pokemonData.loc[my_poke1].loc['hp'])
+        max_hp2 = int(pokemonData.loc[my_poke2].loc['hp'])
+        # set current health to an integer
+        current_hp1 = int(current_hp1)
+        current_hp2 = int(current_hp2)
         
-def readPokemonStats(pokemon, name):
-    pokemon.name = name
-    pokemon.potionCounter = 0
-    pokemon.maxhp = pokemonData.loc[pokemonData['name'] == name]['hp'].values[0]
-    pokemon.hp = pokemon.maxhp
-    pokemon.attack = pokemonData.loc[pokemonData['name'] == name]['attack'].values[0]
-    pokemon.defense = pokemonData.loc[pokemonData['name'] == name]['defense'].values[0]    
-    
-buttons = [widgets.Button(description = name) for name in pokemonData["name"]]
-outputs = [widgets.Output(), widgets.Output()]
+        # check if current hps exceed max, and if so, set to max
+        if current_hp1 > max_hp1:
+            current_hp1 = max_hp1
+        if current_hp2 > max_hp2:
+            current_hp2 = max_hp2
+        
+        # check if current hps are negative, and if so, set to zero
+        if current_hp1 < 0:
+            current_hp1 = 0
+        if current_hp2 < 0:
+            current_hp2 = 0
+        
+        # create 2x2 figure for displaying pokemon and health
+        fig, ax = plt.subplots(2,2, gridspec_kw={'height_ratios': [5,0.5]})
+        # first column is first pokemon and its health bar
+        ax[1,0].set(title = my_poke1.upper())
+        # second column is second pokemon and its health bar
+        ax[1,1].set(title = my_poke2.upper())
 
+        # show pokemon from urls
+        url1 = pokemonData.loc[my_poke1].loc['sprite_url']
+        url2 = pokemonData.loc[my_poke2].loc['sprite_url']
+        im1 = Image.open(requests.get(url1, stream=True).raw)
+        im2 = Image.open(requests.get(url2, stream=True).raw)
+        ax[0,0].imshow(im1)
+        ax[0,1].imshow(im2)
+        
+        # display heart image
+        heart = mpimg.imread('pokemon_heart.png')
+        ax[1,0].imshow(heart, extent=(-13, -1, 0, 10))
+        ax[1,1].imshow(heart, extent=(-13, -1, 0, 10))
+        
+        # turn off axes
+        ax[0,0].axis('off')
+        ax[0,1].axis('off')
+        ax[1,0].axis('off')
+        ax[1,1].axis('off')
+        
+        # set hp colors, green = full health
+        hp_color1 = 'green'
+        hp_color2 = 'green'
+        
+        # change hp colors based on percentage of current pokemon hp
+        if current_hp1 <= max_hp1*0.5:
+            hp_color1 = 'orange'
+        if current_hp1 <= max_hp1*0.1:
+            hp_color1 = 'red'
+        if current_hp2 <= max_hp2*0.5:
+            hp_color2 = 'orange'
+        if current_hp2 <= max_hp2*0.1:
+            hp_color2 = 'red'
+            
+        # health bars for both pokemon
+        ax[1,0].axvspan(0, current_hp1, color = hp_color1)
+        ax[1,1].axvspan(0, current_hp2, color = hp_color2)
+        ax[1,0].axvspan(0, max_hp1, color = hp_color1, alpha = 0.25)
+        ax[1,1].axvspan(0, max_hp2, color = hp_color2, alpha = 0.55)
+        
+        # add amount of remaining health
+        ax[1,0].text(max_hp1 + 2, 1, f"{current_hp1}/{max_hp1}", fontsize = 14, color = hp_color1)
+        ax[1,1].text(max_hp2 + 2, 1, f"{current_hp2}/{max_hp2}", fontsize = 14, color = hp_color2)
+
+        fig.tight_layout()
+        plt.show()
+        
+    except KeyError:
+        print("wrong name")
+        
+    return None
+       
+def readPokemonStats(pokemon, pokemon_name):
+    """
+    COMMENT THIS with function description, args, return, example
+    """
+    # COMMENT BELOW HERE TOO
+    pokemon.pokemon_name = pokemon_name
+    pokemon.potionCounter = 0
+    pokemon.maxhp = int(pokemonData.loc[pokemon_name].loc['hp'])
+    pokemon.hp = int(pokemon.maxhp)
+    pokemon.attack = int(pokemonData.loc[pokemon_name].loc['attack'])
+    pokemon.defense = int(pokemonData.loc[pokemon_name].loc['defense'])
+    
+def takeTurn(attackingPokemon, defendingPokemon):
+    '''
+    Allows users/players to choose/input the moves they want to use against their opponent's Pokemon.
+    Prints amount of damage dealt, amount healed, and whether a Pokemon runs (thus ending the game).
+    
+    Args:
+        attackingPokemon: instance of the pokemon class; the pokemon whose move the user/player inputs
+        defendingPokemon: instance of the pokemon class; the pokemon "receiving" an attack, if attackingPokemon deals damage
+    Returns:
+        None
+    Example:
+        attackingPokemon = pikachu
+        defendingPokemon = charmander
+        Output: "pikachu, choose your move:
+                - run
+                - attack
+                - potion"
+        User Input: attack
+        Output: "pikachu dealt 20 damage to charmander"
+        
+    '''
+    # COMMENT THIS NONSSNESE:
+    try:
+        move = input(f"{attackingPokemon.pokemon_name.upper()}, choose your move:\n- run\n- attack\n- potion\n")
+        if move == "run":
+            attackingPokemon.run(defendingPokemon)
+            raise Exception("Game is over!")
+        elif move == "attack":
+            attackingPokemon.useAttack(defendingPokemon)
+            print(f"{attackingPokemon.pokemon_name.upper()} dealt {attackingPokemon.damage} damage to {defendingPokemon.pokemon_name.upper()}.")
+        elif move == "potion":
+            attackingPokemon.usePotion()
+            print(f"{attackingPokemon.pokemon_name.upper()} has {3 - attackingPokemon.potionCounter} potions left!")
+        else:
+            print("Sorry! Please input a valid move. Your options are run, attack, and potion.")
+            raise ValueError
+    except ValueError:
+        takeTurn(attackingPokemon, defendingPokemon)
+        
 player1pokemon = pokemon()
 player2pokemon = pokemon()
-players = [player1pokemon, player2pokemon]
-desiredPokemonNames = []
 
-for playerNum in range(len(players)):
-    print(f"Player {playerNum+1}, please choose your Pokemon:")
-    for option in range(len(buttons)):
-        display(buttons[option])
-        buttons[option].on_click(on_button_clicked(buttons[option], playerNum))
-        readPokemonStats(players[playerNum], buttons[option].description)
-    display(outputs[playerNum])
-
-print(f"Player 1's pokemon has {player1pokemon.attack} attack points")
-print(f"Player 2's pokemon has {player2pokemon.attack} attack points")
-    #    buttons[option].on_click(on_button_clicked(buttons[option], players[playerNum]))
-    #    print(buttons[option])
-    #    display(output)
+while True:
+    player1pokemon_name = input("Player 1, which Pokemon do you send into battle?\n(If you don't know any, we recommend Charmander, Squirtle, or Bulbasaur!)\n").lower()
     
-#print("Player 2, please choose your Pokemon:")
-#for option in range(len(buttons)):
-#    display(buttons[option])
-#    buttons[option].on_click(on_button_clicked)
-#display(output)
+    try:
+        readPokemonStats(player1pokemon, player1pokemon_name)
+        break
+    except KeyError:
+        print("That Pokemon doesn't exist! Please try again:")
+        
+while True:
+    player2pokemon_name = input("Player 2, which Pokemon do you send into battle?\n(If you don't know any, we recommend Chikorita, Cyndaquil, or Totodile!)\n").lower()
 
-#// let each player choose their pokemon //
-#-- present all pokemon options, raise an error if player 2 chooses the same pokemon as player 1
+    try:
+        readPokemonStats(player2pokemon, player2pokemon_name)
+        break
+    except KeyError:
+        print("That Pokemon doesn't exist! Please try again:")
+    
+turn = 0
 
-#// gameplay loop //
-#turn = 0
-#while both pokemon have health:
-#    if turn is even:
-#        player1's turn
-#    if turn is odd:
-#        player2's turn
-#    turn += 1
-#
-#print("game is over")
-#if turn is even, then player 1 is dead?
-#if turn is odd, then player 2 is dead?
-#
-#takeATurn(player):
-#    present user with attack and heal butttons
-#    if attack:
-#        subtract health from enemy player equal to attack damage
-#    if heal:
-#        if pokemon.hp + 20 > maxhealth
-#            raiseException
-#        add 20 health to player's pokemon
+while int(player1pokemon.hp) > 0 and int(player2pokemon.hp) > 0:
+    
+    display_pokemon(player1pokemon.pokemon_name, player1pokemon.hp, player2pokemon.pokemon_name, player2pokemon.hp)
+    
+    if turn % 2 == 0:
+        takeTurn(player1pokemon, player2pokemon)
+    else:
+        takeTurn(player2pokemon, player1pokemon)
+    turn += 1
+    
+if int(player1pokemon.hp) <= 0:
+    print(f"{player1pokemon.pokemon_name.upper()} has fainted! Player 2 won.")
+else:
+    print(f"{player2pokemon.pokemon_name.upper()} has fainted! Player 1 won.")
+
+display_pokemon(player1pokemon.pokemon_name, player1pokemon.hp, player2pokemon.pokemon_name, player2pokemon.hp)
